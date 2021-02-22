8ab463b9ed315d79d113cc37926b794e0225684a
Cole Roof <ckroof@gmail.com>
2021-02-22 03:00:33 -0500

    Created test to see if linear actuator works

---
+++ LinearActuatorTester/LinearActuatorTester.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+
+#define ACTUATOR 11
+
+Servo actuator;
+uint8_t value;
+
+void setup() {
+    actuator.attach(ACTUATOR, 1000, 2000);
+    pinMode(LED_BUILTIN, OUTPUT);
+    value = 0;
+}
+
+void loop() {
+  actuator.write(value); // write the position for the actuator to go to
+  digitalWrite(LED_BUILTIN, value); // if the value > 0 it will be on, otherwise it'll be off
+  delay(5000); // wait five seconds for the actuator to move
+  
+  if(value) { // flip the value each round
+    value = 0;
+  } else {
+    value = 180;
+  }
+}
